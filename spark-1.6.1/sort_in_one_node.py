398b35e273ab494bc83d0e9507b69e7f88d48ee5
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-23 16:06:34 +0300

    Discarding the master-slave vocabulary

--- spark-1.6.1/sort_in_one_node.py
+++ spark-1.6.1/sort_in_one_node.py
@@ -5,11 +5,11 @@ import shutil,os
 if os.path.exists("../generated_file/result_py"):
     shutil.rmtree("../generated_file/result_py")
 #some of the configuration is set through declaration at spark-defaults.conf
-conf = SparkConf().setAppName("Sorting").setMaster("spark://node-1.testspark.cs331-uc.emulab.net:7077")
+conf = SparkConf().setAppName("Sorting").setMain("spark://node-1.testspark.cs331-uc.emulab.net:7077")
 
 sc = SparkContext(conf=conf)
 text_file = sc.textFile("../generated_file/list_int")
 counts = text_file.map(lambda a : (int(a),a)).sortByKey("true")
 counts.saveAsTextFile("../generated_file/result_py")
 
-# ./bin/spark-submit sort_in_one_node.py --master spark://node-1.testspark.cs331-uc.emulab.net:7077 --deploy-mode cluster --num-executors 1
+# ./bin/spark-submit sort_in_one_node.py --main spark://node-1.testspark.cs331-uc.emulab.net:7077 --deploy-mode cluster --num-executors 1
